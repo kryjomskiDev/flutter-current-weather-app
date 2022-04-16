0d39f26c7b1d540efcddc95d149d0c49aa995a20
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-04-16 22:08:44 +0200

    added keyboard dismisser

---
+++ lib/utils/hide_keyboard.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void hideKeyboard(BuildContext context) {
+  var currentFocus = FocusScope.of(context);
+  if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+    FocusManager.instance.primaryFocus?.unfocus();
+  }
+}
--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -4,6 +4,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wheather_app/generated/l10n.dart';
 import 'package:wheather_app/presentation/pages/home_page.dart';
+import 'package:wheather_app/utils/hide_keyboard.dart';
 
 const _tabletSize = Size(750, 1334);
 const _mobileSize = Size(375, 667);
@@ -12,20 +13,31 @@ class WeatherApp extends StatelessWidget {
   const WeatherApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Device.get().isTablet ? _tabletSize : _mobileSize,
-      builder: () => MaterialApp(
-        localizationsDelegates: const [
-          Strings.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        debugShowCheckedModeBanner: false,
-        supportedLocales: Strings.delegate.supportedLocales,
-        home: const HomePage(),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => _globalUnfocusKeyboard(
+        context: context,
+        child: ScreenUtilInit(
+          designSize: Device.get().isTablet ? _tabletSize : _mobileSize,
+          builder: () => MaterialApp(
+            localizationsDelegates: const [
+              Strings.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            debugShowCheckedModeBanner: false,
+            supportedLocales: Strings.delegate.supportedLocales,
+            home: const HomePage(),
+          ),
+        ),
+      );
+
+  Widget _globalUnfocusKeyboard({
+    required BuildContext context,
+    required Widget child,
+  }) =>
+      GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () => hideKeyboard(context),
+        child: child,
+      );
 }
