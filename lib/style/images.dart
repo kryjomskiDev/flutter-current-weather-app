9b9db735878fe19fd1dee59fa05a2cd6fbac52a8
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-04-17 11:16:52 +0200

    added svg icons and svg setup

---
+++ lib/style/images.dart
@@ -0,0 +1,9 @@
+class IconsSvg {
+  static const _pathPrefix = 'assets/images/svg/icons/';
+  static const weatherCloud = '${_pathPrefix}ic_weather_cloud.svg';
+  static const weatherRain = '${_pathPrefix}ic_weather_rain.svg';
+  static const weatherSun = '${_pathPrefix}ic_weather_sun.svg';
+  static const weatherWind = '${_pathPrefix}ic_weather_wind.svg';
+
+  IconsSvg._();
+}
