11c88e680ae94b79225188dc5b0a1a037f57018b
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-04-25 19:40:08 +0200

    added api url and endpoints

--- lib/data/api_url_provider.dart
+++ lib/data/api_url_provider.dart
@@ -2,5 +2,5 @@ import 'package:injectable/injectable.dart';
 
 @singleton
 class ApiUrlProvider {
-  String getApiUrl() => '';
+  String getApiUrl() => 'https://api.openweathermap.org/data/2.5/';
 }
---
+++ lib/networking_config/networking_endpoints.dart
@@ -0,0 +1,4 @@
+class NetworkingEndpoints {
+  static const getWeatherByCords = 'weather?lat={lat}&lon={lon}&appid={api_key}';
+  static const getWeatherByCityName = 'weather?q={name}&appid={api_key}';
+}
