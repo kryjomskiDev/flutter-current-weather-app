4c6237e04e759859cd2d29a0d3c6ca383517c2ff
Kamil Kryjomski <kryjomskicontact@gmail.com>
2022-05-25 17:32:46 +0200

    refactor

--- lib/style/images.dart
+++ lib/style/images.dart
@@ -1,10 +1,5 @@
 class IconsSvg {
   static const _pathPrefix = 'assets/images/svg/icons/';
-  static const weatherCloud = '${_pathPrefix}ic_weather_cloud.svg';
-  static const weatherRain = '${_pathPrefix}ic_weather_rain.svg';
-  static const weatherSun = '${_pathPrefix}ic_weather_sun.svg';
-  static const weatherWind = '${_pathPrefix}ic_weather_wind.svg';
-
   static const location = '${_pathPrefix}location.svg';
   static const search = '${_pathPrefix}search.svg';
   static const settings = '${_pathPrefix}settings.svg';
